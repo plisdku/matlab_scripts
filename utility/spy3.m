6d6def9243dba11d374789de6ab4d8b9a4722b1e
pch <pch@7d6175ef-f675-4c24-bb17-d6b158afa8cd>
2011-06-14 18:16:24 +0000

    Adding spy3().
    git-svn-id: svn+ssh://transfer.stanford.edu/afs/ir/users/p/c/pch/repo/matlab@530 7d6175ef-f675-4c24-bb17-d6b158afa8cd

---
+++ utility/spy3.m
@@ -0,0 +1,15 @@
+function spy3(matrix, varargin)
+% spy3(matrix, varargin)
+
+[ii, jj, vv] = find(matrix);
+
+if nargin > 1
+    plot3(jj,ii,vv, varargin{:});
+else
+    plot3(jj,ii,vv,'.', 'MarkerSize', 1);
+end
+
+axis ij
+view(2)
+xlabel(sprintf('nz = %i', length(vv)));
+
