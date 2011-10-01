ceabe7ba2ebf442d60fb17ee0530a41b329fa9c9
pch <pch@7d6175ef-f675-4c24-bb17-d6b158afa8cd>
2011-10-01 04:42:21 +0000

    Updates to OutputFile yeeCells and positions, both t5 and t6, to make them... correct...
    git-svn-id: svn+ssh://transfer.stanford.edu/afs/ir/users/p/c/pch/repo/matlab@586 7d6175ef-f675-4c24-bb17-d6b158afa8cd

--- fdtd_scripts/+t5/@OutputFile/yeeCells.m
+++ fdtd_scripts/+t5/@OutputFile/yeeCells.m
@@ -5,9 +5,9 @@ jj = cell(size(obj.Regions));
 kk = cell(size(obj.Regions));
 
 for rr = 1:length(obj.Regions)
-    ii{rr} = obj.Regions{rr}.YeeCells(1):obj.Regions{rr}.YeeCells(4);
-    jj{rr} = obj.Regions{rr}.YeeCells(2):obj.Regions{rr}.YeeCells(5);
-    kk{rr} = obj.Regions{rr}.YeeCells(3):obj.Regions{rr}.YeeCells(6);
+        ii{rr} = obj.Regions{rr}.YeeCells(1):obj.Regions{rr}.Stride(1):obj.Regions{rr}.YeeCells(4);
+        jj{rr} = obj.Regions{rr}.YeeCells(2):obj.Regions{rr}.Stride(2):obj.Regions{rr}.YeeCells(5);
+        kk{rr} = obj.Regions{rr}.YeeCells(3):obj.Regions{rr}.Stride(3):obj.Regions{rr}.YeeCells(6);
 end
 
 if length(obj.Regions) == 1
