ff027725dc9a6040791d8779af0b5ae264c778eb
pch <pch@7d6175ef-f675-4c24-bb17-d6b158afa8cd>
2012-10-23 17:31:34 +0000

    Put fminnaive4 into the repos.  It's called, simply, fminnaive.
    git-svn-id: svn+ssh://transfer.stanford.edu/afs/ir/users/p/c/pch/repo/matlab@741 7d6175ef-f675-4c24-bb17-d6b158afa8cd

--- utility/complex2rgb.m
+++ utility/complex2rgb.m
@@ -1,13 +1,22 @@
-function rgb = complex2rgb(A, valueFunc)
+function rgb = complex2rgb(A, varargin)
+
+X.Mode = 'value';
+X = parseargs(X, varargin{:});
 
 colVec = @(v) reshape(v, [], 1);
 
-hue = (pi+angle(colVec(A)))/2/pi;
-saturation = ones(size(hue));
-value = abs(colVec(A)) / max(abs(colVec(A)));
-%value = saturation;
-
-%saturation = value;
-%value = ones(size(value));
-
+if strcmpi(X.Mode, 'value')
+    hue = (pi+angle(colVec(A)))/2/pi;
+    saturation = ones(size(hue));
+    value = abs(colVec(A)) / max(abs(colVec(A)));
+elseif strcmpi(X.Mode, 'saturation')
+    hue = (pi+angle(colVec(A)))/2/pi;
+    saturation = abs(colVec(A)) / max(abs(colVec(A)));
+    value = ones(size(saturation));
+elseif strcmpi(X.Mode, 'flat')
+    hue = (pi+angle(colVec(A)))/2/pi;
+    saturation = ones(size(hue));
+    value = saturation;
+end
+    
 rgb = reshape(hsv2rgb(cat(2, hue, saturation, value)), [size(A), 3]);
