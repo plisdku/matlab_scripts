5dd54d9a6333bbc2277930c6fc6e4eb9f9b453e6
Paul Hansen <pch@stanford.edu>
2013-05-27 20:16:03 -0700

    Revamped orangecrush.m a little bit.

--- utility/orangecrush.m
+++ utility/orangecrush.m
@@ -1,4 +1,4 @@
-function colormap = orangecrush(varargin)
+function [colormap, scale] = orangecrush(varargin)
 
 if nargin > 0
     exponent = varargin{1};
@@ -6,10 +6,27 @@ else
     exponent = 1;
 end
 
-colormap = zeros(256, 3);
+if nargin > 1
+    numColors = varargin{2};
+else
+    numColors = 256;
+end
 
-topHalf = 129:256;
-bottomHalf = 1:128;
+if rem(numColors, 2) == 0
+    numColors = numColors + 1;
+end
+
+numHalfColors = floor(numColors/2);
+
+colormap = zeros(numColors, 3);
+
+%topHalf = 129:256;
+%bottomHalf = 1:128;
+
+vv = linspace(0, 1, numColors);
+
+topHalf = vv(vv > 0.5);
+bottomHalf = vv(vv < 0.5);
 
 %{
 color1 = [0.4118, 0.1922, 1];
@@ -20,16 +37,34 @@ color40 = [
 
 % 1.  Set the top colors to orange
 
-colormap(topHalf,1) = (0:127)/127;
-%colormap(topHalf,2) = 0.2720 * ( ((0:31)/31) - 0.4*((0:31)/31).^3 )  ;
-colormap(topHalf,2) = 0.3 * ( ((0:127)/127) ).^2 ;
-%colormap(topHalf, 3) = 0.3*((0:31)/31).^3;
+vals = linspace(0, 1, numHalfColors+1);
+
+%valTop = 0.5*(vals + vals.^exponent);
+valTop = vals.^exponent;
+
+valTop = valTop(2:end);
+
+colormap(vv > 0.5, 1) = valTop;
+colormap(vv > 0.5, 2) = 0.3 * valTop.^2;
+
+%colormap(topHalf,1) = (0:127)/127;
+%%colormap(topHalf,2) = 0.2720 * ( ((0:31)/31) - 0.4*((0:31)/31).^3 )  ;
+%colormap(topHalf,2) = 0.3 * ( ((0:127)/127) ).^2 ;
+%%colormap(topHalf, 3) = 0.3*((0:31)/31).^3;
 
 
 % 2.  Set the bottom colors to blue
 
-colormap(bottomHalf,1) = 0.4118 * ((127:-1:0)/127);
-colormap(bottomHalf,2) = 0.1922 * ((127:-1:0)/127).^2;
-colormap(bottomHalf,3) = (127:-1:0)/127;
+valBot = valTop(end:-1:1);
 
-colormap = colormap .^ exponent;
+colormap(vv < 0.5,1) = 0.4118 * valBot;
+colormap(vv < 0.5,2) = 0.1922 * valBot.^2;
+colormap(vv < 0.5,3) = valBot;
+
+%colormap(bottomHalf,1) = 0.4118 * ((127:-1:0)/127);
+%colormap(bottomHalf,2) = 0.1922 * ((127:-1:0)/127).^2;
+%colormap(bottomHalf,3) = (127:-1:0)/127;
+
+%colormap = colormap .^ exponent;
+
+scale = [valBot 0 valTop];
